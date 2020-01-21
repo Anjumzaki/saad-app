2b22ccd8a9e2f5ff6165ca897a1c438e9f8fcb8d
anjumzaki <anjumzaki8@gmail.com>
2020-01-22 03:42:11 +0500

    :df]

--- src/SignUp.js
+++ src/SignUp.js
@@ -5,6 +5,7 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import {Button} from 'reactstrap'
 import MyNavbar from './MyNavbar'
 export default class Login extends React.Component {
     constructor(props) {
@@ -35,8 +36,9 @@ export default class Login extends React.Component {
                         <input type="text" id="login" class="fadeIn second" value={this.state.lName} onChange={this.handleChange} name="lName" placeholder="Last Name" />
                         <input type="text" id="login" class="fadeIn second" value={this.state.userName} onChange={this.handleChange} name="userName" placeholder="User Name" />
                         <input type="text" id="password" class="fadeIn third" value={this.state.password} onChange={this.handleChange} name="password" placeholder="Password" />
-                        <input type="submit" onClick={() => console.log("ho")} class="fadeIn fourth" value="Log In" />
                     </form>
+                    <Button classname="btn btn-primary" onClick={() => console.log("ho")} class="fadeIn fourth"  > Sign Up</Button>
+
                     <div id="formFooter">
                         <a class="underlineHover" href="/Login">Already have an Account? Login Here!</a>
                     </div>
